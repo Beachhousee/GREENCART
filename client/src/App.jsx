603db16b9444ae95c9073ae730ab824fdde77253
rijans <makwanarijans@gmail.com>
2025-07-26 23:12:53 +0530

    Cart-functionalities(Add,remove,delete,and payment-option)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,51 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import { Route,Routes, useLocation } from 'react-router-dom'
-import MainBanner from './components/MainBanner'
-import Home  from './pages/Home'
-import {Toaster} from "react-hot-toast"
-import Footer from './components/Footer'
-import { useAppContext } from './context/AppContext'
-import Login from './components/Login'
-import AllProducts from './pages/AllProducts'
-import ProductCategory from './pages/ProductCategory'
+import React from "react";
+import NavBar from "./components/NavBar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import MainBanner from "./components/MainBanner";
+import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+import { useAppContext } from "./context/AppContext";
+import Login from "./components/Login";
+import AllProducts from "./pages/AllProducts";
+import ProductCategory from "./pages/ProductCategory";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
 
 const App = () => {
-
-  const isSellerPath =useLocation().pathname.includes("seller")
-  const {showUserLogin}=useAppContext()
+  const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin } = useAppContext();
   return (
     <div>
-  {isSellerPath ? null :<NavBar></NavBar>}
-  {showUserLogin? <Login></Login> :null}
-  <Toaster></Toaster>
-<div className={`${isSellerPath}? "" : "px-6 md:px-16 lg:px-24 xl:px-32"`}>
-  <Routes>
-    <Route path='/' element={<Home></Home>}> </Route>
-    <Route path= '/products'element ={<AllProducts></AllProducts>}></Route>
-    <Route path ='/products/:category' element={<ProductCategory></ProductCategory>}></Route>
-  </Routes>
-</div>
+      {isSellerPath ? null : <NavBar></NavBar>}
+      {showUserLogin ? <Login></Login> : null}
+      <Toaster></Toaster>
+      <div
+        className={`${isSellerPath}? "" : "px-6 md:px-16 lg:px-24 xl:px-32"`}
+      >
+        <Routes>
+          <Route path="/" element={<Home></Home>}>
+            {" "}
+          </Route>
+          <Route path="/products" element={<AllProducts></AllProducts>}></Route>
+          <Route
+            path="/products/:category"
+            element={<ProductCategory></ProductCategory>}
+          ></Route>
+          <Route
+            path="/products/:category/:id"
+            element={<ProductDetails></ProductDetails>}
+          ></Route>
+             <Route
+            path="/cart"
+            element={<Cart></Cart>}
+          ></Route>
+        </Routes>
+      </div>
 
-{!isSellerPath && <Footer></Footer> }
+      {!isSellerPath && <Footer></Footer>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -55,6 +55,26 @@ export const AppContextProvider = ({ children }) => {
     setCartItems(cartData)
   };
 
+const getCartCount = () => {
+  let totalCount = 0;
+  for (const item in cartItems) {
+    totalCount += cartItems[item];
+  }
+  return totalCount;
+}
+
+// Get Cart Total Amount
+const getCartAmount = () => {
+  let totalAmount = 0;
+  for (const items in cartItems) {
+    let itemInfo = products.find((product) => product._id === items);
+    if (cartItems[items] > 0) {
+      totalAmount += itemInfo.offerPrice * cartItems[items];
+    }
+  }
+  return Math.floor(totalAmount * 100);
+}
+
 
   useEffect(() => {
     fetchProducts();
@@ -75,7 +95,8 @@ export const AppContextProvider = ({ children }) => {
     updateCartItem,
     removeFromCart ,
     cartItems,
-    searchQuery,setSearchQuery
+    searchQuery,setSearchQuery,
+    getCartAmount,getCartCount
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
