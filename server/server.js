cd01b8ca12a68393838de05e130405b3e1a30308
rijans <makwanarijans@gmail.com>
2025-07-31 12:05:31 +0530

    Backend Structure

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors'
+import connectDB from './configs/db.js';
+import userRouter from './routes/userRoute.js'; 
+import 'dotenv/config'
+
+const app = express();
+
+const port = process.env.PORT || 4000; 
+await connectDB()
+
+const allowedOrigins = ['http://localhost:5173']
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }))
+
+app.get('/', (req, res) => res.send("api is working"));
+app.use('/api/user', userRouter)
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
