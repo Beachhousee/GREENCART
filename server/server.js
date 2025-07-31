b9bb422baf191684d945e1c9c3d511d75765703c
rijans <makwanarijans@gmail.com>
2025-07-31 16:59:30 +0530

    Seller Login,Logout and Authentication

--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ import cors from 'cors'
 import connectDB from './configs/db.js';
 import userRouter from './routes/userRoute.js'; 
 import 'dotenv/config'
+import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/Cloudinary.js';
 
 const app = express();
 
 const port = process.env.PORT || 4000; 
 await connectDB()
+await connectCloudinary()
 
 const allowedOrigins = ['http://localhost:5173']
 
@@ -18,6 +21,7 @@ app.use(cors({ origin: allowedOrigins, credentials: true }))
 
 app.get('/', (req, res) => res.send("api is working"));
 app.use('/api/user', userRouter)
+app.use('/api/seller',sellerRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
